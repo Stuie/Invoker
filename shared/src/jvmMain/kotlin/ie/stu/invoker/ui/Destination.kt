f20821bf45c8f16ad32d46a87712d459921f6d66
Stuart Gilbert <stuart.gilbert@gmail.com>
2026-07-26 12:40:44 -0700

    Proper icon

--- shared/src/jvmMain/kotlin/ie/stu/invoker/ui/Destination.kt
+++ shared/src/jvmMain/kotlin/ie/stu/invoker/ui/Destination.kt
@@ -2,15 +2,15 @@ package ie.stu.invoker.ui
 
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.composables.icons.materialsymbols.MaterialSymbols
-import com.composables.icons.materialsymbols.outlined.Deck
 import com.composables.icons.materialsymbols.outlined.Group
 import com.composables.icons.materialsymbols.outlined.Home
 import com.composables.icons.materialsymbols.outlined.Info
+import com.composables.icons.materialsymbols.outlined.Playing_cards
 import com.composables.icons.materialsymbols.outlined.Settings
 
 enum class Destination(val label: String, val icon: ImageVector) {
     Home(Strings.HOME, MaterialSymbols.Outlined.Home),
-    Decks(Strings.DECKS, MaterialSymbols.Outlined.Deck),
+    Decks(Strings.DECKS, MaterialSymbols.Outlined.Playing_cards),
     Settings(Strings.SETTINGS, MaterialSymbols.Outlined.Settings),
     Community(Strings.COMMUNITY, MaterialSymbols.Outlined.Group),
     About(Strings.ABOUT, MaterialSymbols.Outlined.Info),
